3e08900aef2506ea324d7c9394cb2a0399a8a8e3
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-24 10:55:38 +0800

    Update Controller.php

--- config/Controller.php
+++ config/Controller.php
@@ -6,13 +6,11 @@
     switch($lokasi){
       case "alamadmin.php":
             echo 'CRUID bagian sini';
-            break;
+            
         case "login.php":
             echo 'resya bagian sini';
-            break;
         case "info.php":
             echo 'gerin bagian sini';
-             break;
         
         
     }
