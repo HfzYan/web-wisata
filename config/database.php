026cbb94493ed532e96a433c8d1f20a0a0fa2550
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-24 10:52:48 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,8 @@
+<?php
+	$server = "us-cdbr-east-04.cleardb.com";
+	$user = "b2a147e23ecf94";
+	$pass = "18cef105";
+	$database = "heroku_c2f277e1c35a3e7";
 
+	$koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
+?>
