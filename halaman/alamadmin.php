a910a282f407e6dea861d848ee6eb641042c2293
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-24 16:44:04 +0800

    Update alamadmin.php

--- halaman/alamadmin.php
+++ halaman/alamadmin.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-<?php //include '../config/Controller.php' ?>
+<?php include '../config/database.php' ?>
+
+<?php $fetch = mysqli_query($koneksi,"SELECT * FROM wisata_alam;"); ?>
 
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Alam Admin</title>
-	<link rel="stylesheet" type="text/css" href="../css/alamadmin.css">
+	<link rel="stylesheet" type="text/css" href="../amazingkalsel/css/alamadmin.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 </head>
-<body >
+<body class="modal-open">
 
 <div class="wrapper">
     <div class="sidebar">
-        <h2><img src="../img/logoamazingkalselputih.png"></a></h2>
+        <h2><img src="../img/logoamazingkalselputih.png"></h2>
         <div class="menu">
           <ul>
             <li><a href="berandaadmin.php"><img src="../img/admin/icon-home.png">Home</a></li>
@@ -44,8 +46,8 @@
         <table class="content-table">
         <thead>
           <tr>
-            <th>NO</th>
-            <th>JUDUL</th>
+            <th>ID</th>
+            <th>NAMA</th>
             <th>ALAMAT</th>
             <th>DESKRIPSI</th>
             <th>GAMBAR</th>
@@ -56,8 +58,10 @@
           <tr>
             
             
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <!-- MODAL TAMBAH -->
+
+            
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
               <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                   <div class="modal-header">
@@ -67,7 +71,7 @@
                     </button>
                   </div>
                   <div class="modal-body">
-                    ...
+                    <?php echo 'add'; ?>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -76,32 +80,23 @@
                 </div>
               </div>
             </div>
+
+
+
           </tr>
+          <?php while($data = mysqli_fetch_array($fetch)): ?>
           <tr>
-            <td width="3%">id</td>
-            <td width="40%">judul</td>
-            <td><a href=alamadmin.php?id=1 data-toggle="modal" data-target="#exampleModal">Alamat</a></td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Deskripsi</a></td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Gambar</a></td>
-            <td><button class="editbutton"data-toggle="modal" data-target="#exampleModal">Edit</button><button class="hapusbutton">Hapus</button></td>
+            
+            <td width="3%"><?php echo $data['id'] ?></td>
+            <td width="40%"><?php echo $data['nama'] ?></td>
+            <td><a href="tesalamadmin.php?id=2" data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalAlamat">Alamat</a></td>
+            <td><a href=# data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalDeskripsi">Deskripsi</a></td>
+            <td><a href=# data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalGambar">Gambar</a></td>
+            <td><button class="editbutton"data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalEdit">Edit</button><button class="hapusbutton">Hapus</button></td>
           </tr>
+          <?php include '/modal/modal-tabel.php' ?>
+          <?php endwhile ?>
         <!-- <tr class="active-row"> -->
-          <tr>
-            <td width="3%">id</td>
-            <td width="40%">judul</td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Alamat</a></td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Deskripsi</a></td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Gambar</a></td>
-            <td><button class="editbutton"data-toggle="modal" data-target="#exampleModal">Edit</button><button class="hapusbutton">Hapus</button></td>
-          </tr>
-          <tr>
-            <td width="3%">id</td>
-            <td width="40%">judul</td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Alamat</a></td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Deskripsi</a></td>
-            <td><a href=# data-toggle="modal" data-target="#exampleModal">Gambar</a></td>
-            <td><button class="editbutton" data-toggle="modal" data-target="#exampleModal">Edit</button><button class="hapusbutton">Hapus</button></td>
-          </tr>
         </tbody>
         </table>
     </div>
