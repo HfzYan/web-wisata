be030c6500320fb2cc6dbd604b18ecfbe67ea47b
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-24 16:44:30 +0800

    Create modal-tabel.php

---
+++ halaman/modal/modal-tabel.php
@@ -0,0 +1,83 @@
+            
+            <!-- MODAL EDIT -->
+            <div class="modal fade" id="<?php echo $data['id'] ?>ModalEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
+              <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Objek Wisata Alam</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <?php echo $data['id'] ?>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- MODAL ALAMAT -->
+            <div class="modal fade" id="<?php echo $data['id'] ?>ModalAlamat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
+              <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Alamat <?php echo $data['nama'] ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <?php echo $data['alamat'] ?>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </div> 
+              </div>
+            </div>
+
+            <!-- MODAL DESKRIPSI -->
+            <div class="modal fade" id="<?php echo $data['id'] ?>ModalDeskripsi" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
+              <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Deskripsi <?php echo $data['nama'] ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <?php echo $data['deskripsi'] ?>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- MODAL GAMBAR -->
+            <div class="modal fade" id="<?php echo $data['id'] ?>ModalGambar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
+              <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Gambar <?php echo $data['nama'] ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <img src="<?php echo $data['gambar'] ?>" height=600 width=600>
+                    <hr>
+                    <img src="<?php echo $data['gambar_2'] ?>" height=600 width=600>
+
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
