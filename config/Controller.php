be6be76569dea2ca66c5484e0258e0b8101f4148
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-24 10:56:45 +0800

    Update Controller.php

--- config/Controller.php
+++ config/Controller.php
@@ -3,15 +3,16 @@
 
     $lokasi = basename($_SERVER['PHP_SELF']);
     //Pembagian halaman
-    switch($lokasi){
-      case "alamadmin.php":
-            echo 'CRUID bagian sini';
+   // switch($lokasi){
+    //  case "alamadmin.php":
+    //        echo 'CRUID bagian sini';
             
-        case "login.php":
-            echo 'resya bagian sini';
-        case "info.php":
-            echo 'gerin bagian sini';
+    //    case "login.php":
+    //        echo 'resya bagian sini';
+    //    case "info.php":
+    //        echo 'gerin bagian sini';
         
         
-    }
-    ?>
+   // }
+echo 'tes';
+?>
