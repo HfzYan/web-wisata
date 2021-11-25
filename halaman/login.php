6532620bef8fafdf8c2243a55074f0b7742b0bc7
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-25 10:24:44 +0800

    Update login.php

--- halaman/login.php
+++ halaman/login.php
@@ -39,7 +39,7 @@ session_start();
             </form>
             <?php
             if(isset($_POST['login'])){
-              include"koneksi.php";
+              include "../config/database.php";
               $username = $_POST['Username'];
               $password = $_POST['Password'];
 
