8967eab2a411a0b7fc174c11d36731a909865463
Gerin Rahardi <90899852+Grahardian@users.noreply.github.com>
2021-12-03 20:30:27 +0800

    Update and rename infoalam11.php to inputalam.php
    halaman digunakan untuk memasukkan data baru pada kategori alam

---
+++ halaman/inputalam.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<?php include '../config/database.php' ?>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Input Data</title>
+	<link rel="stylesheet" type="text/css" href="../css/edit.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+<div class="wrapper">
+	<div class="main_content">
+		<div class="header1">
+          <b class="judul">Input Info Wisata Alam</b> 
+          <b class="admin">Admin</b><button type="button">Logout</button>
+        </div>
+	</div>
+</div>
+
+<br>
+<div class="container">
+	<div class="card" style="box-shadow: 0 5px 10px rgba(65, 60, 60, 0.2);">
+  		<div class="card-header" style="background-color: #7C1212">
+    		<label></label>
+  		</div>
+  		<div class="card-body">
+    		<form action="" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <label>Gambar Wisata</label>
+            <input type="file" name="gambar" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Nama Wisata</label>
+            <input type="text" class="form-control" name="nama">
+          </div>
+          <div class="form-group">
+            <label>Deskripsi</label>
+            <textarea class="form-control" type="text" name="deskripsi" rows="7"></textarea>
+          </div>
+          <div class="form-group">
+            <label>Alamat</label>
+            <br>
+            <textarea type="text" name="alamat" class="form-control" rows="4"></textarea>
+          </div>
+				<br>
+        <p align="right">
+				  <button class="btn btn-success" type="submit" name="save" value="save">Simpan</button>
+			  </p>
+      </form>
+      <?php 
+        if(isset($_POST['save'])) {
+          $name = $_POST['nama'];
+          $desc = $_POST['deskripsi'];
+          $loc = $_POST['alamat'];
+          $pic = $_FILES['gambar']['name'];
+          $folder = '../img/wisata-alam/';
+          move_uploaded_file($folder, $pic);
+          $data = mysqli_query($koneksi,"INSERT INTO wisata_alam (nama, alamat, gambar, deskripsi) VALUES ('$name', '$loc', '$folder/$pic', '$desc')");
+          if ($data) {
+            echo "<script>document.location= '../halaman/alamadmin.php';</script>";
+          }
+          else{
+            echo mysqli_error($koneksi);
+          }
+        }
+      ?>
+		</div>
+	</div>
+</div>
+
+<br><br>
+			<!--input data selesai-->
+</body>
+</html>
