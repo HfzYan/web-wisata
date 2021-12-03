b80a60f302c82d6dfaa592ae93135ef535b1dd75
Gerin Rahardi <90899852+Grahardian@users.noreply.github.com>
2021-12-03 20:38:03 +0800

    Update alamadmin.php

--- halaman/alamadmin.php
+++ halaman/alamadmin.php
@@ -42,16 +42,11 @@ if($_SESSION["login"] <> 1){
 		</a>
 
         </div>
-         <button type="button" class="addbutton" data-toggle="modal" data-target="#exampleModal">
-          + Tambah
-        </button> 
-        <a href='alam.php' target="_blank">
-        <button type="button" class="addbutton" href=#>
-          Tampilan Halaman Alam ->
-        </button> 
-        </a>
+	    
+         <a href="../halaman/inputalam.php"><button type="button" class="addbutton">+ Tambah</button></a>
+         <a href='alam.php' target="_blank"><button type="button" class="addbutton" href=#>Tampilan Halaman Alam -></button></a>
         
-        <table class="content-table">
+        <table class="content-table" data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0">
         <thead>
           <tr>
             <th>ID</th>
@@ -63,117 +58,36 @@ if($_SESSION["login"] <> 1){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            
-            
-            <!-- MODAL TAMBAH -->
-
-            
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
-              <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Tambah Objek Wisata Alam</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <?php echo 'add'; ?>
-                    <form action="namauntukaction.php" class="container">
-                    <label for="email"><b>Judul</b></label><br>
-                    <input type="text" placeholder="Isi dengan nama wisata alam" name="judul" required>
-                    <br><br>
-
-                    <label for="alamat"><b>Alamat</b></label><br>
-                    <textarea name="alamat" placeholder="Isi dengan alamat wisata alam" rows="5" cols="40"></textarea>
-                    <br><br>
-
-                    <label for="deskripsi"><b>Deskripsi</b></label><br>
-                    <textarea name="deskripsi" placeholder="Isi dengan penjelasan detail tentang objek wisata alam" rows="5" cols="40"></textarea>
-                    <br><br>
-
-                    <label for="img">Gambar</label><br>
-                    <input type="file" id="img" name="img" accept="image/*">
-                    <br>
-                    <button type="submit" class="editbutton">Login</button>
-                    </form>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-
-          </tr>
+          <?php $nomor=1; ?>
           <?php while($data = mysqli_fetch_array($fetch)): ?>
           <tr>
             
-            <td width="3%"><?php echo $data['id'] ?></td>
+            <td width="3%"><?php echo $nomor ?></td>
             <td width="40%"><?php echo $data['nama'] ?></td>
-            <?php if(isset($data['alamat'])) { ?>
-              <td><a href="#" data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalAlamat">Alamat</a></td>
-            <?php } 
-              else {?>
-              <td>-</td>
-            <?php } ?>
+            <td><a href="tesalamadmin.php?id=2" data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalAlamat">Alamat</a></td>
             <td><a href=# data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalDeskripsi">Deskripsi</a></td>
             <td><a href=# data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalGambar">Gambar</a></td>
-            <td><button class="editbutton"data-toggle="modal" data-target="#<?php echo $data['id'] ?>ModalEdit">Edit</button><button class="hapusbutton">Hapus</button></td>
+            <td>
+              <a href="../halaman/editalam.php?halaman=editalam&id=<?=$data['id'];?>" name="edit"><button class="editbutton" >Edit</button></a>
+              <a href="../halaman/hapusalam.php?halaman=hapusalam&id=<?=$data['id'];?>"  onclick="return confirm('Yakin Hapus?')"><button type="button" class="hapusbutton">Hapus</button></a>
+            </td>
           </tr>
-          <?php include 'modal/modal-tabel.php'; 
-	
-	//CRUD
-		if(isset($_POST['edit'])){
-      			$id = $_POST['editid'];
-      			$gambar = $_POST['editgambar'];
-      			$nama = $_POST['editnama'];
-      			$deskripsi = $_POST['editdeskripsi'];
-      			$alamat = $_POST['editalamat'];
-
-      			if(isset($_POST['editgambar'])){
-        			$edit = mysqli_query($koneksi, "UPDATE wisata_alam set
-                              		gambar = '$gambar',
-                              		nama = '$nama',
-                              		deskripsi = '$deskripsi',
-                              		alamat = '$alamat'
-                              		WHERE id = '$id'");
-      			}
-      			else{
-        			$edit = mysqli_query($koneksi, "UPDATE wisata_alam set
-                              	nama = '$nama',
-                              	deskripsi = '$deskripsi',
-                              	alamat = '$alamat'
-                              	WHERE id = '$id'");
-      			}
-			
-
-                            if($edit){
-                              echo "<script>alert('Data berhasil diubah!');
-                              document.location= '../halaman/alamadmin.php';
-                              </script>";
-                            }
-                            else
-                            {
-                              echo "<script>alert('Data gagal diubah!');
-                              document.location= '../halaman/alamadmin.php';
-                              </script>";
-                            }
-		} 
-		   //AKHIR CRUD 
-		?>
-		
-		
+          
+          <!-- Modal Data -->
+          <?php include 'modal/modal-tabel.php';           
+          ?>
+            
+          <!-- AKHIR CRUD -->
+          <?php $nomor++; ?>
           <?php endwhile ?>
+
         <!-- <tr class="active-row"> -->
         </tbody>
-        </table>
+    </table>
+        <br><br>
     </div>
 </div>
+	
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
