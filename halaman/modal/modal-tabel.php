3ca52745bde75b7d2ef7f8fec61484649665ba83
Gerin Rahardi <90899852+Grahardian@users.noreply.github.com>
2021-12-03 20:44:54 +0800

    Update modal-tabel.php

--- halaman/modal/modal-tabel.php
+++ halaman/modal/modal-tabel.php
@@ -1,58 +1,4 @@
-<!-- MODAL EDIT -->
-            <div class="modal fade" id="<?php echo $data['id'] ?>ModalEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
-              <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Data <?php echo $data['nama'] ?></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <div class="container">
-                      <div class="card">
-                        <div class="card-body">
-                          <form method="post" enctype="multipart/form-data">
-                          <div class="form-group">
-                              <label>ID</label>
-                              <input type="text" class="form-control" name="editid" value="<?php echo $data['id'] ?>" readonly>
-                            </div>
-                            <div class="form-group">
-                             <label>Gambar Wisata</label>
-                               <input type="file" name="editgambar" class="form-control">
-                               <br>
-                                <img src="<?php echo $data['gambar'] ?>" height="60" width="60">
-                            </div>
-                            <div class="form-group">
-                              <label>Nama Wisata</label>
-                              <input type="text" class="form-control" name="editnama" value="<?php echo $data['nama'] ?>">
-                            </div>
-                            <div class="form-group">
-                              <label>Deskripsi</label>
-                              <textarea class="form-control" name="editdeskripsi" rows="7"><?php echo $data['deskripsi'] ?></textarea>
-                            </div>
-                            <div class="form-group">
-                              <label>Alamat</label>
-                              <br>
-                              <textarea type="text" name="editalamat" class="form-control" rows="7"><?php echo $data['alamat'] ?></textarea>
-                            </div>
-                                      <br><br>
-                            <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                     <button type="submit" class="btn btn-success" name="edit" id="#<?php echo $data['id'] ?>ModalEdit">Edit Data</button>
-                             </div>
-                            <br>
-                         </form>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-            
+           
 
             <!-- MODAL ALAMAT -->
             <div class="modal fade" id="<?php echo $data['id'] ?>ModalAlamat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
@@ -106,9 +52,6 @@
                   </div>
                   <div class="modal-body">
                     <img src="<?php echo $data['gambar'] ?>" height=600 width=600>
-                    <hr>
-                    <img src="<?php echo $data['gambar_2'] ?>" height=600 width=600>
-
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
