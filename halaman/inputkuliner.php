702217c7399eda8db1bd802fc052540b82e8445a
Gerin Rahardi <90899852+Grahardian@users.noreply.github.com>
2021-12-08 09:40:04 +0800

    Update inputkuliner.php

--- halaman/inputkuliner.php
+++ halaman/inputkuliner.php
@@ -64,7 +64,11 @@
           $loc = $_POST['alamat'];
           $pic = $_FILES['gambar']['name'];
           $folder = '../img/wisata-kuliner/';
-          move_uploaded_file($folder, $pic);
+	  $lokasigambar = $_FILES['gambar']['tmp_name'];
+          $folder = '../img/wisata-alam/';
+          $data = mysqli_query($koneksi,"INSERT INTO wisata_alam (nama, alamat, gambar, deskripsi) VALUES ('$name', '$loc', '$folder/$pic', '$desc')");
+          if ($data) {
+          move_uploaded_file($lokasigambar, $folder.$pic);
           $data = mysqli_query($koneksi,"INSERT INTO wisata_kuliner (nama, alamat, gambar, deskripsi) VALUES ('$name', '$loc', '$folder/$pic', '$desc')");
           if ($data) {
             echo "<script>document.location= '../halaman/kulineradmin.php';</script>";
