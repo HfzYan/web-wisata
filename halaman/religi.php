2081dd03aaa59c36a7f7f8b9973e17ac1c6bdd16
aryopratama100 <90923023+aryopratama100@users.noreply.github.com>
2021-12-16 22:22:07 +0800

    update responsive religi.php

--- halaman/religi.php
+++ halaman/religi.php
@@ -6,6 +6,8 @@
 <html lang="en">
 <head>
 	<title>Amazing Kalsel - Religi</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- menghubungkan dengan file css -->
 	<link rel="stylesheet" type="text/css" href="../css/objek-wisata.css">
 </head>
